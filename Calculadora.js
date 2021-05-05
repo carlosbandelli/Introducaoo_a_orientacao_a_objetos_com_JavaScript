1094c518531682cad1fda316736ab3e89eb8510d
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-05-04 22:44:03 -0300

    this
    sobre  o this

---
+++ Calculadora.js
@@ -0,0 +1,16 @@
+class Calculadora{
+
+    static Somar(a,b){
+        console.log(a+b)
+    }
+
+    static Sub(a,b){
+        console.log(a-b)
+    }
+}
+// para usar os metodos de uma calculadora precisava fazer isso
+// var calc = new Calculadora()
+// calc.Somar(2,2);
+// calc.Sub(2,2) 
+Calculadora.Somar(10,20);
+Calculadora.Sub(10,520)
--- Filme.js
+++ Filme.js
@@ -1,5 +1,8 @@
+
+//CLASS
 class Filme{  //Nome da classe definida
     constructor( titulo, ano, genero, diretor, duracao){ // atributos da classe em javascript, tem que ter exatamente esse nome, tem que colocar isso para definir o atributo, dentro deel voce coloca o parametro que suas variaveis de atributo vão receber
+        //ATRIBUTOS
         this.titulo = titulo; //Atributos são como variaveis, só que pertencem a uma classe.Parar criar um, é bem semelhante ao processo de criação de variavel, com a adição do 'this.' antes do nome do atributo
         this.ano = ano;
         this.genero = genero;                   //Contructor:  é basicamente um funçãi especial que é responsavel  
@@ -8,7 +11,8 @@ class Filme{  //Nome da classe definida
         this.atores = [];
 
     } //para diferenciar o this.titulo = titulo, é o this, toda a variavel que é precedida por ele é atributo e o que elea recebe é a varialvel do parametro 
-
+    
+    //  MÈTODOS
     Reproduzir(){ // metodos são simplesmente como função, o importante é deixa os metodos com a primeira letra maiuscula
         console.log("Reproduzindo....")
     }
@@ -24,11 +28,19 @@ class Filme{  //Nome da classe definida
     Fechar(){
         console.log("Fechar X")
     }
+
+    Ficha(){
+        console.log("Título:"  + this.titulo) // com esse this estamos acessando os atributos da clase dentro do metodo da classe, sempre que chamar esse função ela vai pegar o objeto que esta esse atributo igual 
+        console.log("Ano de lançamento:" + this.ano);
+        console.log("Genero: " + this.genero);
+        this.Reproduzir()// para chamr um metodo dentro de outro metodo que esta dentro da classse é só usar e o this na frente
+    }
+
 }
 
 var vingadores = new Filme("Vingadores 2", 2014, "Ação", "Alguem", "2h"); // quando se colo esse new e chama a classe significa que ele está pegando a classe filme e fazendo ela nova com atributos de valores novos, sem nada do anterior, só pegar a estrutura sem valores
 
-
+vingadores.Ficha()
 
 console.log("Título do filme:" + vingadores.titulo);
 console.log("Ano de lançamento:" + vingadores.ano);
